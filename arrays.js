d50cd5c0231b23b833ebb19e2e1ac0d362059b74
Megan Poosangtongchai <mpoosangtongchai@gmail.com>
2018-02-08 06:10:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,6 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(2);
+  return array.slice(0, array.length - 1);
 }
 
